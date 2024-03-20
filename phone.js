a5fd3ae8ba09b92a9a79458dd0aa5011e6418f45
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-03-20 18:48:09 +0600

    loading dots add

--- phone.js
+++ phone.js
@@ -43,19 +43,35 @@ const displayPhones = phones =>{
 
         phoneContainer.appendChild(phoneCard)
     });
+    
+        toggleLoadingDot(false);
 }
 
 const handleSearch = () =>{
+    toggleLoadingDot(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText);
     loadPhone(searchText);
 }
 const handleSearch2 = () =>{
+    toggleLoadingDot(true);
     const searchField = document.getElementById('search-field2');
     const searchText = searchField.value;
     // console.log(searchText);
-    loadPhone(searchText)
+    loadPhone(searchText);
+}
+
+// loading Dot
+const toggleLoadingDot = (isLoading) =>{
+    const loadingDots = document.getElementById('loading-dots');
+    if(isLoading){
+        loadingDots.classList.remove('hidden');
+    }
+    else{
+        
+        loadingDots.classList.add('hidden');
+    }
 }
 
 // loadPhone()
